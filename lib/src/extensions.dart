4dcc70f0c09c880d5f31cc09e5931635064349ce
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-09-15 00:34:50 +0530

    dependencies updated

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -4,6 +4,6 @@ extension GetxViewExtensions<T> on GetView<T> {
   T get c => controller;
 }
 
-extension GetxWidgetExtensions<T extends GetLifeCycleBase?> on GetWidget<T> {
+extension GetxWidgetExtensions<T extends GetLifeCycle> on GetWidget<T> {
   T get c => controller;
 }
